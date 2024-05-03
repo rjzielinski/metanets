050fe742ce934516741b22c310d2e23d56999a84
Robert Zielinski <rjz1996@outlook.com>
2024-05-02 20:59:03 -0400

    fix errors

--- dvae.py
+++ dvae.py
@@ -1,8 +1,11 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 import nibabel as nib
 import nilearn
 import numpy as np
-import os
 import pandas as pd
 import seaborn as sns
 import torch
@@ -10,11 +13,8 @@ import torch.distributions
 import torch.nn.functional as F
 import torch.utils
 import torchvision
-
-from dataclasses import dataclass
-from datetime import datetime
 from nilearn import datasets, plotting
-from nilearn.maskers import NiftiMasker, NiftiMapsMasker
+from nilearn.maskers import NiftiMapsMasker, NiftiMasker
 from sklearn.model_selection import ShuffleSplit
 from sklearn.preprocessing import StandardScaler
 from torch import nn
@@ -32,37 +32,48 @@ labels = atlas["labels"]
 
 data = nilearn.datasets.fetch_development_fmri(data_dir=data_dir)
 
+
 class fMRIDataset(torch.utils.data.Dataset):
-    def __init__(self, img_dir, labels_file=None, transform=None, target_transform=None):
+    def __init__(
+        self, img_dir, labels_file=None, transform=None, target_transform=None
+    ):
         if labels_file is not None:
-            self.img_labels = pd.read_csv(labels_file) 
+            self.img_labels = pd.read_csv(labels_file)
         else:
             self.img_labels = None
         self.img_dir = img_dir
-        self.img_files = [name for name in os.listdir(img_dir) if (os.path.isfile(os.path.join(img_dir, name)) and name.endswith('.gz'))]
+        self.img_files = [
+            name
+            for name in os.listdir(img_dir)
+            if (os.path.isfile(os.path.join(img_dir, name)) and name.endswith(".gz"))
+        ]
         self.transform = transform
         self.target_transform = target_transform
-    
+
     def __len__(self):
         return len(self.img_files)
-    
+
     def __getitem__(self, idx):
+
         img_path = os.path.join(self.img_dir, self.img_files[idx])
         image = nilearn.image.get_data(img_path)
+        image_norm = (image - np.min(image)) / (np.max(image) - np.min(image))
         if self.img_labels is not None:
             label = self.img_labels.iloc[idx, 1]
             if self.target_transform:
                 label = self.target_transform(label)
         else:
-            label = np.array([]) 
+            label = np.array([])
         if self.transform:
-            image = self.transform(image)
+            image_norm = self.transform(image_norm)
         else:
-            image = torch.tensor(image)
-        return image, label
+            image_norm = torch.tensor(image_norm)
+
+        return image_norm, label
+
 
 development_fmri = fMRIDataset(img_dir="data/development_fmri/development_fmri")
-batch_size = 5
+batch_size = 1
 test_split = 0.2
 shuffle_dataset = True
 random_seed = 42
@@ -79,15 +90,12 @@ train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_indices)
 test_sampler = torch.utils.data.sampler.SubsetRandomSampler(test_indices)
 
 train_loader = torch.utils.data.DataLoader(
-    development_fmri,
-    batch_size=batch_size,
-    sampler=train_sampler
+    development_fmri, batch_size=batch_size, sampler=train_sampler
 )
 test_loader = torch.utils.data.DataLoader(
-    development_fmri,
-    batch_size=batch_size,
-    sampler=test_sampler
+    development_fmri, batch_size=batch_size, sampler=test_sampler
 )
+
 
 @dataclass
 class DVAEOutput:
@@ -127,6 +135,7 @@ class DVAE(nn.Module):
             nn.Sigmoid(),
             nn.Unflatten(1, input_dim),
         )
+        self.double()
 
     def encode(self, x, eps: float = 1e-8):
         x = self.encoder(x)
@@ -135,10 +144,10 @@ class DVAE(nn.Module):
         scale_tril = torch.diag_embed(scale)
 
         return torch.distributions.MultivariateNormal(mu, scale_tril=scale_tril)
-    
+
     def reparametrize(self, dist):
         return dist.rsample()
-    
+
     def decode(self, z):
         return self.decoder(z)
 
@@ -154,24 +163,26 @@ class DVAE(nn.Module):
                 x_hat=x_hat,
                 loss=None,
                 loss_recon=None,
-                loss_kl=None
+                loss_kl=None,
             )
-        
-        loss_recon = F.binary_cross_entropy(x_hat, x + 0.5, reduction="none").sum(-1).mean()
+
+        loss_recon = F.binary_cross_entropy(x_hat, x, reduction="none").sum(-1).mean()
         std_normal = torch.distributions.MultivariateNormal(
             torch.zeros_like(z, device=z.device),
-            scale_tril=torch.eye(z.shape[-1], device=z.device).unsqueeze(0).expand(z.shape[0], -1, -1),
+            scale_tril=torch.eye(z.shape[-1], device=z.device)
+            .unsqueeze(0)
+            .expand(z.shape[0], -1, -1),
         )
         loss_kl = torch.distributions.kl.kl_divergence(dist, std_normal).mean()
         loss = loss_recon + loss_kl
 
         return DVAEOutput(
-            z_dist = dist,
+            z_dist=dist,
             z_sample=z,
             x_hat=x_hat,
             loss=loss,
             loss_recon=loss_recon,
-            loss_kl=loss_kl
+            loss_kl=loss_kl,
         )
 
 
@@ -192,18 +203,23 @@ def train(model, dataloader, optimizer, prev_updates, writer=None):
                 if p.grad is not None:
                     param_norm = p.grad.data.norm(2)
                     total_norm += param_norm.item() ** 2
-            total_norm = total_norm ** (1. / 2)
+            total_norm = total_norm ** (1.0 / 2)
 
-            print(f'Step {n_upd:,} (N samples: {n_upd*batch_size:,}), Loss: {loss.item():.4f} (Recon: {output.loss_recon.item():.4f}, KL: {output.loss_kl.item():.4f}) Grad: {total_norm:.4f}')
+            print(
+                f"Step {n_upd:,} (N samples: {n_upd*batch_size:,}), Loss: {loss.item():.4f} (Recon: {output.loss_recon.item():.4f}, KL: {output.loss_kl.item():.4f}) Grad: {total_norm:.4f}"
+            )
             if writer is not None:
                 global_step = n_upd
-                writer.add_scalar('Loss/Train', loss.item(), global_step)
-                writer.add_scalar('Loss/Train/BCE', output.loss_recon.item(), global_step)
-                writer.add_scalar('Loss/Train/KLD', output.loss_kl.item(), global_step)
-                writer.add_scalar('GradNorm/Train', total_norm, global_step)
+                writer.add_scalar("Loss/Train", loss.item(), global_step)
+                writer.add_scalar(
+                    "Loss/Train/BCE", output.loss_recon.item(), global_step
+                )
+                writer.add_scalar("Loss/Train/KLD", output.loss_kl.item(), global_step)
+                writer.add_scalar("GradNorm/Train", total_norm, global_step)
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
     return prev_updates + len(dataloader)
+
 
 def test(model, dataloader, cur_step, writer=None):
     model.eval()
@@ -212,7 +228,7 @@ def test(model, dataloader, cur_step, writer=None):
     test_kl_loss = 0
 
     with torch.no_grad():
-        for data, target in tqdm(dataloader, desc='Testing'):
+        for data, target in tqdm(dataloader, desc="Testing"):
             data = data.to(device)
             # data = data.view(data.size(0), -1)
             output = model(data, compute_loss=True)
@@ -220,23 +236,27 @@ def test(model, dataloader, cur_step, writer=None):
             test_loss += output.loss.item()
             test_recon_loss += output.loss_recon.item()
             rest_kl_loss += output.loss_kl.item()
-    
+
     test_loss /= len(dataloader)
     test_recon_loss /= len(dataloader)
     test_kl_loss /= len(dataloader)
-    print(f'====> Test set loss: {test_loss:.4f} (BCE: {test_recon_loss:.4f}, KLD: {test_kl_loss:.4f})')
+    print(
+        f"====> Test set loss: {test_loss:.4f} (BCE: {test_recon_loss:.4f}, KLD: {test_kl_loss:.4f})"
+    )
 
     if writer is not None:
-        writer.add_scalar('Loss/Test', test_loss, global_step=cur_step)
-        writer.add_scalar('Loss/Test/BCE', output.loss_recon.item(), global_step=cur_step)
-        writer.add_scalar('Loss/Test/KLD', output.loss_kl.item(), global_step=cur_step)
+        writer.add_scalar("Loss/Test", test_loss, global_step=cur_step)
+        writer.add_scalar(
+            "Loss/Test/BCE", output.loss_recon.item(), global_step=cur_step
+        )
+        writer.add_scalar("Loss/Test/KLD", output.loss_kl.item(), global_step=cur_step)
 
-        writer.add_images('Test/Reconstructions', output.x_hat, global_step=cur_step)
-        writer.add_images('Test/Originals', data, global_step=cur_step)
+        writer.add_images("Test/Reconstructions", output.x_hat, global_step=cur_step)
+        writer.add_images("Test/Originals", data, global_step=cur_step)
 
         z = torch.randn(16, latent_dim).to(device)
         samples = model.decode(z)
-        writer.add_images('Test/Samples', samples, global_step=cur_step)
+        writer.add_images("Test/Samples", samples, global_step=cur_step)
 
 
 input_dim = next(iter(train_loader))[0].shape[1:5]
@@ -247,12 +267,22 @@ num_epochs = 50
 latent_dim = 2
 hidden_dim = 32
 
-model = DVAE(input_dim=input_dim, input_size=input_size, hidden_dim=hidden_dim, latent_dim=latent_dim)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+model = DVAE(
+    input_dim=input_dim,
+    input_size=input_size,
+    hidden_dim=hidden_dim,
+    latent_dim=latent_dim,
+)
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=learning_rate, weight_decay=weight_decay
+)
 writer = SummaryWriter(f'runs/dvae_{datetime.now().strftime("%Y%m%d-%H%M%S")}')
 
 prev_updates = 0
 for epoch in range(num_epochs):
-    print(f'Epoch {epoch+1}/{num_epochs}')
+    print(f"Epoch {epoch+1}/{num_epochs}")
     prev_updates = train(model, train_loader, optimizer, prev_updates, writer=writer)
     test(model, test_loader, prev_updates, writer=writer)
+
+
+print(writer)
